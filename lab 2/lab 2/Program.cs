87caccb4a978e06919b63d57fcc2fcee6b327ba6
tooba26162 <233518@students.au.edu.pk>
2024-11-10 13:48:27 +0500

    Add files via upload

---
+++ lab 2/lab 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //activity1
+            Console.WriteLine($"there are {args.Length} arguments ");
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            //activity 2
+            Console.WriteLine("Hello world");
+            Console.WriteLine("I am a BSCS student.");
+
+            //activity 3
+            
+            Console.Write("enter year: ");
+            int year=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(year);
+
+        }
+       
+    }
+}
